0045ccaaf2c7e64700d6e84ab09824111fe3b419
dev2kod <devesh4coding@gmail.com>
2025-11-01 22:50:16 +0530

    x

--- tailwind/src/Components/Container.jsx
+++ tailwind/src/Components/Container.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Container = () => {
   return (
     <div class='wrapper'>
-        Container
+        this file would contain the main components and designs
     </div>
   )
 }
